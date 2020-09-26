0efcc0858e670155d82daf02851cc37a31be0a25
Matei Mircea <mrc_matei@yahoo.com>
2020-09-26 22:05:22 +0300

    Updates

---
+++ precache-manifest.0e3b26b97b2d2ff476240255fdb59a44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd77eba25757e0a00851ef4769fc0a14",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "1080781835d95904be07",
+    "url": "/todo/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "669069b49d1185feff16",
+    "url": "/todo/static/css/main.be9cd417.chunk.css"
+  },
+  {
+    "revision": "1080781835d95904be07",
+    "url": "/todo/static/js/2.878365e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.878365e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "669069b49d1185feff16",
+    "url": "/todo/static/js/main.ae657e26.chunk.js"
+  },
+  {
+    "revision": "f482dafe70f147ebfd71",
+    "url": "/todo/static/js/runtime-main.6ddef9be.js"
+  }
+]);
